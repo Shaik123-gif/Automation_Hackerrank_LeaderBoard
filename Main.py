2a1078031e558d0b4c04685be5ccbd749798127a
Shaik Ismail <122015096@sastra.ac.in>
2020-10-19 15:55:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import pandas as pd
+import pandas as pd  #For File Handling
 
+#For inserting the LeaderBoard Names in the List
 def printName(name):
     i = 0
     for a in soup.findAll("div", attrs={'class': 'span-flex-4'}):
@@ -10,6 +11,7 @@ def printName(name):
         i = i + 1
     print(name)
 
+#For inserting the LeaderBoard Scores in the List    
 def printScore(score):
     i = 0
     for a in soup.findAll("div", attrs={'class': 'span-flex-3'}):
@@ -21,7 +23,7 @@ def printScore(score):
 driver1 = webdriver.Chrome()
 name1 = []
 score1 = []
-driver1.get('https://www.hackerrank.com/contests/scanit-weekly-contest-23-aug-2020/leaderboard')
+driver1.get('https://www.hackerrank.com/contests/scanit-weekly-contest-23-aug-2020/leaderboard')  #Link of the Particular Site (LeaderBoard 1)
 content1 =driver1.page_source
 soup = BeautifulSoup(content1,features="html.parser")
 printName(name1)
@@ -32,7 +34,7 @@ df1 = pd.DataFrame({'Name':name1,'Score':score1})
 driver2 = webdriver.Chrome()
 name2 = []
 score2 = []
-driver2.get('https://www.hackerrank.com/contests/scanit-weekly-contest-23-aug-2020/leaderboard/2')
+driver2.get('https://www.hackerrank.com/contests/scanit-weekly-contest-23-aug-2020/leaderboard/2')  #Link of the Particular Site (LeaderBoard 2)
 content2 = driver2.page_source
 soup = BeautifulSoup(content2,features="html.parser")
 printName(name2)
@@ -43,7 +45,7 @@ df1 = df1.append(df2, ignore_index=True)
 driver3 = webdriver.Chrome()
 name3 = []
 score3 = []
-driver3.get('https://www.hackerrank.com/contests/scanit-weekly-contest-23-aug-2020/leaderboard/3')
+driver3.get('https://www.hackerrank.com/contests/scanit-weekly-contest-23-aug-2020/leaderboard/3') #Link of the Particular Site (LeaderBoard 3)
 content3 =driver3.page_source
 soup = BeautifulSoup(content3,features="html.parser")
 printName(name3)
@@ -52,6 +54,6 @@ df3 = pd.DataFrame({'Name':name3,'Score':score3})
 
 df1 = df1.append(df3, ignore_index=True)
 
-df1.to_csv('Week2.csv',index=False,encoding="utf-8")
+df1.to_csv('Excel_Sheet.csv',index=False,encoding="utf-8")
 
 
